804a7f9d8e5677d71b2fba0dbdf24cd3057e76f9
Carlos <carloshoyos@gmail.com>
2022-07-07 16:19:39 -0400

    problem 36 - faster solution

--- python/036.py
+++ python/036.py
@@ -13,18 +13,47 @@ leading zeros.)
 """
 
 # computers are fast..  just a little bit of glue and int <-> string conversion and bin function to convert base. 
+# a way to speed this up is to not test all numbers for palindromic, but to start by generating all palindromic numbers (concat abc + cba )
 
 def isPalindromic(num):
     return str(num) == str(num)[::-1]
 
-
-print( sum( [i for i in range(1,1000000) if isPalindromic(i) and isPalindromic(str(bin(i))[2:]) ]))
-
+def getMultiBasePalindromic_all(max):
+    return ( sum( [i for i in range(1, max) if isPalindromic(i) and isPalindromic(str(bin(i))[2:]) ]))
 
 # the non pythonic way to do this
+# total = 0
 # for i in range(1, 1000000):    # no trailing zeros 
 #     if isPalindromic(i) and isPalindromic(str(bin(i))[2:]):
 #         print(i, bin(i))
+#         total += i
+# print(total, "\n\n")
 
 
 
+# the faster way to do this by generating the palindromic numbers by merging abc + cba (for even length), and abc + m + cba (for odd length) numbers
+# TODO: max length is hardcoded, make it a parameter
+
+def getMultiBasePalindromic_gen():
+    total = 0
+
+    tens = [str(x) for x in range(1, 100)] 
+    hun = [str(x) for x in range(100, 1000)] 
+
+    for i in tens + hun:    # even digits 
+        j = int(str(i) +  str(i)[::-1])
+        if isPalindromic(str(bin(j))[2:]):
+            total += j
+
+    for i in tens + ['']:    # odd digits 
+        for m in range(0,10):
+            j = int(str(i) +  str(m) + str(i)[::-1])
+            if isPalindromic(str(bin(j))[2:]):
+                total += j
+
+    return total
+
+
+
+print(getMultiBasePalindromic_gen())
+print(getMultiBasePalindromic_all(1000000))
